9f4dd12f46bc1e1b68fd7c85d46b0df99773e318
ozodbek26 <ozodbek200017@gmail.com>
2025-01-29 15:31:02 +0600

    add font

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import About from "./pages/About";
 import MyData from "./componebt3/MyData/MyData";
 import Settings from "./componebt3/Settings/Settings";
+import ChangePassword from "./componebt4/ChangePassword/ChangePassword";
 
 function App() {
   return (
@@ -15,12 +16,15 @@ function App() {
           <Link to="/about" className="nav-link">About</Link>
           <Link to="/mydata" className="nav-link">MyData</Link>
           <Link to="/settings" className="nav-link">Settings</Link>
+          <Link to="/ChangePassword" className="nav-link">ChangePassword</Link>
+
         </div>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<About />} />
           <Route path="/mydata" element={<MyData />} />
-          <Route path="/settings" element={<Settings/>} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/ChangePassword" element={<ChangePassword />} />
 
         </Routes>
       </Router>
